1709d0f7c5bb0cb4f4716d5f3402be6ff2c1488a
Ayush Pandey <ayushpandey8439@gmail.com>
2021-09-09 16:09:47 +0200

    Remove partitioning

--- src/gingko_sup.erl
+++ src/gingko_sup.erl
@@ -5,23 +5,23 @@
 -export([init/1]).
 
 start_link() ->
-  supervisor:start_link(gingko_sup, [testpartition]).
+  supervisor:start_link(gingko_sup, []).
 
-init([Partition]) ->
+init(_Args) ->
     Worker = {?LOGGING_MASTER,
-    {?LOGGING_MASTER, start_link, [integer_to_list(Partition)++"op_journal", none]},
+    {?LOGGING_MASTER, start_link, ["main_log", none]},
     permanent, 5000, worker, [?LOGGING_MASTER]},
 
   CacheDaemon = {?CACHE_DAEMON,
-    {?CACHE_DAEMON,start_link,[list_to_atom(integer_to_list(Partition)++"gingko_cache"), 2 , 200000]},
+    {?CACHE_DAEMON,start_link,[gingko_cache, 2 , 200000]},
     permanent,5000,worker,[?CACHE_DAEMON]},
 
   CheckpointDaemon = {checkpoint_daemon,
-    {checkpoint_daemon,start_link,[list_to_atom(integer_to_list(Partition)++"gingko_checkpoint_store")]},
+    {checkpoint_daemon,start_link,[gingko_checkpoint_store]},
     permanent,5000,worker,[checkpoint_daemon_server]},
 
   LogIndexDaemon = {?LOG_INDEX_DAEMON,
-    {?LOG_INDEX_DAEMON,start_link,[list_to_atom(integer_to_list(Partition)++"gingko_log_index")]},
+    {?LOG_INDEX_DAEMON,start_link,[gingko_log_index]},
     permanent,5000,worker,[?LOG_INDEX_DAEMON]},
 
   SupFlags = #{strategy => one_for_one, intensity => 1, period => 5},
